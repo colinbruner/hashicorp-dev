dd50be472ec04f1470b9b5c9e23971a186dc2ff0
Colin Bruner <colin.d.bruner@gmail.com>
2022-11-01 13:06:31 -0400

    fix incorrect address to nomad secret engine

--- modules/vault/secret/nomad/main.tf
+++ modules/vault/secret/nomad/main.tf
@@ -2,6 +2,6 @@
 resource "vault_nomad_secret_backend" "config" {
   backend     = "nomad"
   description = "Nomad secret backend"
-  address     = "http://127.0.0.1:8500"
+  address     = "http://127.0.0.1:4646"
   token       = var.nomad_token
 }
